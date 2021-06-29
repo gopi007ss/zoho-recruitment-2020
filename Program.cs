41dc10b1e40a724b9ab1d29afc6b0dac1bbb697b
gopi007ss <31753990+gopi007ss@users.noreply.github.com>
2021-06-29 11:49:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int i, j, n;
+			string str = Console.ReadLine();
+			n = str.Length;
+			int midindex = n/2;
+			char[] arr = str.ToCharArray();
+			for (i = 1; i <= n; i++)
+			{
+				for (j = i; j < n; j++)
+					Console.Write(" ");
+				int k = 0;
+				for (j = 0; j < i; j++)
+                {					
+					if(j > midindex)
+                    {
+						Console.Write(arr[k]);
+						k++;
+					}					
+					else
+						Console.Write(arr[n-midindex + j -1]);
+				}
+					
+				
+				Console.WriteLine();
+			}
+		}
+	}
+}
